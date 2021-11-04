994cebdfde7b3a4c31dfbb1fd8202aa0ec49446e
dclleite <eduappsplay@gmail.com>
2021-11-03 23:55:44 -0300

    Metodos de manipulação da transição e executar máquina

--- Maquina.py
+++ Maquina.py
@@ -17,4 +17,75 @@ class Maquina():
         self.E = "11"
         self.REJEITA = "\nPalavra rejeitada\n" 
         self.ACEITA = "\nPalavra aceita\n"
-        self.ultima_transicao = []             
+        self.ultima_transicao = []      
+
+    def buscarTransicao(self):
+
+        estado_atual = self.fita2
+
+        transicoes = [transicao for transicao in self.lista_transicoes if transicao.estado_inicial == estado_atual]
+        
+        if transicoes == []:
+            return False, self.ultima_transicao
+        
+        for transicao in transicoes:
+            if transicao.simbolo_entrada == self.fita3[self.cabesss_maquina]:
+                try:
+                    self.ultima_transicao = transicao
+                    self.executarTransicao(transicao)
+                    return True, self.ultima_transicao
+                except ErroMaquina:
+                    print(self.REJEITA)
+                    exit(0)
+        
+        print(self.REJEITA)
+        exit(0)
+
+    def executarTransicao(self, transicao):
+
+        self.fita2 = transicao.estado_destino
+        self.fita3[self.cabesss_maquina] = transicao.simbolo_escrita
+        if transicao.direcao == self.D:
+            self.cabesss_maquina += 1
+        else:
+            self.cabesss_maquina -= 1
+            if self.cabesss_maquina < 0:
+                raise ErroMaquina 
+
+    def executarMaquina(self):
+        print("INICIO :")
+        print( self )
+        i = 1
+        var = self.buscarTransicao()
+        conta_transicao = dict()
+
+        while var[0] :
+            if i > (len( self.fita3 ) ** len( self.fita1 ) ):
+                print(self.REJEITA)
+                print("possivel loop infinito")
+                exit(0)
+
+            try:
+                conta_transicao[var[1]] +=1
+            except KeyError:
+                conta_transicao[var[1]] = 0
+
+            maior_ocorrencia = max( conta_transicao , key = conta_transicao.get )
+
+            if conta_transicao[maior_ocorrencia] == var[1] and maior_ocorrencia > ( len( self.fita3 ) ** len( self.fita1) ):
+                print(self.REJEITA)
+                print("possivel loop infinito")
+                exit(0)
+
+            print("Passo " + str(i) )
+            print(self)	
+            i+=1
+
+            var = self.buscarTransicao()
+            pass
+        
+
+        print(self.ACEITA)
+
+class ErroMaquina(Exception):
+    pass       
